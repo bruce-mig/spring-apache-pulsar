baeebc7a18d9bb71d5690cd0249efa41d916046e
bruce-mig <brucetmig@icloud.com>
2025-07-06 15:44:25 +0200

    added enable pulsar annotation

--- producer/src/main/java/com/github/bruce_mig/spring_apache_pulsar/producer/ProducerApplication.java
+++ producer/src/main/java/com/github/bruce_mig/spring_apache_pulsar/producer/ProducerApplication.java
@@ -2,8 +2,10 @@ package com.github.bruce_mig.spring_apache_pulsar.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.pulsar.annotation.EnablePulsar;
 
 @SpringBootApplication
+@EnablePulsar
 public class ProducerApplication {
 
 	public static void main(String[] args) {
