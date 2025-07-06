580e072869d506d09621f8b568d9ec29f2ae3bff
bruce-mig <brucetmig@icloud.com>
2025-07-06 15:23:27 +0200

    first commit

---
+++ producer/src/test/java/com/github/bruce_mig/spring_apache_pulsar/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bruce_mig.spring_apache_pulsar.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
