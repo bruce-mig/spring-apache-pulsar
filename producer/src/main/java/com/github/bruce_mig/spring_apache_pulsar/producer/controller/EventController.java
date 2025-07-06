9e284e043e6768d266be67ed17be85adc8271a88
bruce-mig <brucetmig@icloud.com>
2025-07-06 15:44:37 +0200

    added event controller

---
+++ producer/src/main/java/com/github/bruce_mig/spring_apache_pulsar/producer/controller/EventController.java
@@ -0,0 +1,22 @@
+package com.github.bruce_mig.spring_apache_pulsar.producer.controller;
+
+import com.github.bruce_mig.spring_apache_pulsar.producer.EventPublisher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/producer")
+public class EventController {
+
+    private final EventPublisher publisher;
+
+    public EventController(EventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @GetMapping("/text/{message}")
+    public ResponseEntity<String> sendTextEvent(@PathVariable String message){
+        publisher.publishPlainMessage(message);
+        return ResponseEntity.accepted().body("Message Published");
+    }
+}
